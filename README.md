d09944a9b90da358b8449dc1e9fcd22dc964352b
= <=>
2022-10-27 16:45:55 -0400

    main, src

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# dxd-auto-voter
+## DXD Auto Voter
+
+### Setup
+
+```bash
+cd ~
+git clone https://github.com/ledgerleapllc/dxd-auto-voter.git
+cd dxd-auto-voter
+sudo apt update
+sudo apt -y install software-properties-common
+sudo add-apt-repository ppa:ondrej/php
+sudo apt update
+sudo apt-get install -y php8.1
+sudo apt-get install -y php8.1-{bcmath,gd,mbstring,common,curl}
+```
+
+### Configure
+
+Configure **accounts.json** and add accounts there. Include email/passwords under "accounts" array in the following format,
+
+```json
+"accounts": [
+	{
+		"email":    "account1@example.com",
+		"password": "password123"
+	},
+	...
+]
+```
+
+This file is ignored by github.
+
+### Run Auto Voter
+
+```bash
+php main.php
+```
+
+Script will load accounts from **accounts.json** and cast a "for" vote valued at 0.01 rep on all available ballots for each account.
+
+Create a daily cronjob for fully autonomous voting.
+
+```
+0 12 * * * php ~/dxd-auto-voter/main.php
+```
---
+++ src/AutoVoter.php
@@ -0,0 +1,178 @@
+<?php
+
+class AutoVoter {
+	static function elog($msg) {
+		file_put_contents('php://stderr', print_r("\n", true));
+		file_put_contents('php://stderr', '[DXD AutoVoter '.(date('c')).'] - ');
+		file_put_contents('php://stderr', print_r($msg, true));
+	}
+
+	static function get_accounts() {
+		$content  = file_get_contents(__DIR__.'/../accounts.json');
+		$json     = json_decode($content);
+		$accounts = $json->accounts ?? array();
+
+		foreach ($accounts as &$account) {
+			if ($account->email == 'example@test.com') {
+				$account->email    = '';
+				$account->password = '';
+			} else {
+				self::elog('Auto voting for account: '.$account->email);
+			}
+		}
+
+		return $accounts;
+	}
+
+	static function request_post(
+		$url         = '', 
+		$token       = '', 
+		$post_fields = array()
+	) {
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_fields));
+		$headers   = array();
+		$headers[] = 'Content-Type: application/json';
+		$headers[] = 'Authorization: Bearer '.$token;
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		$result    = curl_exec($ch);
+		curl_close($ch);
+		return json_decode($result);
+	}
+
+	static function request_get(
+		$url   = '', 
+		$token = ''
+	) {
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+		$headers   = array();
+		$headers[] = 'Authorization: Bearer '.$token;
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		$result    = curl_exec($ch);
+		curl_close($ch);
+		return json_decode($result);
+	}
+
+	static function vote(
+		$proposal_id,
+		$vote_id,
+		$direction = "for",
+		$token,
+		$value = 0.01
+	) {
+		$response = self::request_post(
+			"https://backend.devxdao.com/api/user/vote",
+			$token,
+			$post_fields = array(
+				"proposalId" => $proposal_id,
+				"voteId"     => $vote_id,
+				"type"       => $direction,
+				"value"      => $value
+			)
+		);
+
+		return $response;
+	}
+
+	public static function run() {
+		$accounts = self::get_accounts();
+
+		foreach ($accounts as $account) {
+			$global_token = '';
+
+			if(
+				$account->email &&
+				$account->password
+			) {
+				$response = self::request_post(
+					"https://backend.devxdao.com/api/login",
+					$global_token,
+					$post_fields = array(
+						"email"    => $account->email,
+						"password" => $account->password
+					)
+				);
+
+				$global_token = $response->user->accessTokenAPI ?? '';
+				self::elog($global_token."\n");
+			}
+
+			if($global_token) {
+				$informal_ballots = self::request_get(
+					'https://backend.devxdao.com/api/shared/active-informal-votes', 
+					$global_token
+				);
+
+				$votes = $informal_ballots->votes ?? array();
+
+				foreach($votes as $vote) {
+					$vote_result_type = $vote->vote_result_type ?? null;
+
+					if(!$vote_result_type) {
+						$proposalId = $vote->proposalId ?? 0;
+						$timeLeft   = $vote->timeLeft ?? 0;
+						$title      = $vote->title ?? '';
+						$voteId     = $vote->id ?? 0;
+
+						self::elog("Informal Proposal ".$proposalId);
+						self::elog($title);
+						self::elog("Voting 'For'...");
+
+						$vote_response = self::vote(
+							$proposalId,
+							$voteId,
+							"for",
+							$global_token,
+							0.1
+						);
+
+						sleep(4);
+						self::elog("Success\n");
+					}
+				}
+
+
+				$formal_ballots = self::request_get(
+					'https://backend.devxdao.com/api/shared/active-formal-votes', 
+					$global_token
+				);
+
+				$votes = $formal_ballots->votes ?? array();
+
+				foreach($votes as $vote) {
+					$vote_result_type = $vote->vote_result_type ?? null;
+
+					if(!$vote_result_type) {
+						$proposalId = $vote->proposalId ?? 0;
+						$timeLeft   = $vote->timeLeft ?? 0;
+						$title      = $vote->title ?? '';
+						$voteId     = $vote->id ?? 0;
+
+						self::elog("Formal Proposal ".$proposalId);
+						self::elog($title);
+						self::elog("Voting 'For'...");
+
+						$vote_response = self::vote(
+							$proposalId,
+							$voteId,
+							"for",
+							$global_token,
+							0.1
+						);
+
+						sleep(4);
+						self::elog("Success\n");
+					}
+				}
+			}
+		}
+
+		self::elog("Done\n");
+	}
+}
